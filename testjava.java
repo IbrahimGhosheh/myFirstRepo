8ed39058e7ac42f25598a6eb5fe3a5b8c45b71d4
Ibrahim Ghosheh <ibrahimghosheh2000@gmail.com>
2022-02-15 16:18:14 +0200

    from intellij...

--- testjava.java
+++ testjava.java
@@ -2,4 +2,7 @@ public class testjava {
     public static void main(String[] args) {
         System.out.println("testing GIT");
     }
+    private void hi(int i){
+        System.out.println(i);
+    }
 }
